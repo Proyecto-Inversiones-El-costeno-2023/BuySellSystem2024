0457b159eae0b4da50c561d977501a3cc6c57815
LunaRama <aramirezengineer@gmail.com>
2024-10-14 10:13:49 -0400

    Base de datos 1.1

--- Base de datos/sistema_compraventa1.sql
+++ Base de datos/sistema_compraventa1.sql
@@ -1,5 +1,5 @@
 -- --------------------------------------------------------
--- Host:                         127.0.0.1
+-- Host:                         localhost
 -- Versión del servidor:         11.5.2-MariaDB - mariadb.org binary distribution
 -- SO del servidor:              Win64
 -- HeidiSQL Versión:             12.8.0.6908
@@ -14,23 +14,23 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Volcando datos para la tabla sistema_compraventa.categorias: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.cliente: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.c_venta: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.d_venta: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.entrada: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.productos: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.proveedor: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.salida: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
--- Volcando datos para la tabla sistema_compraventa.usuarios: ~0 rows (aproximadamente)
+-- La exportación de datos fue deseleccionada.
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
