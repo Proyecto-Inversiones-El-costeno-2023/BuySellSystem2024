bab61f5c813f22f5bd7ec2f7e66ab1a65fe6887c
LunaRama <aramirezengineer@gmail.com>
2024-10-13 22:58:21 -0400

    Base de datos

---
+++ Base de datos/sistema_compraventa.sql
@@ -0,0 +1,39 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Versión del servidor:         11.5.2-MariaDB - mariadb.org binary distribution
+-- SO del servidor:              Win64
+-- HeidiSQL Versión:             12.8.0.6908
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+-- Volcando datos para la tabla sistema_compraventa.categorias: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.cliente: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.c_venta: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.d_venta: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.entrada: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.productos: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.proveedor: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.salida: ~0 rows (aproximadamente)
+
+-- Volcando datos para la tabla sistema_compraventa.usuarios: ~0 rows (aproximadamente)
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
